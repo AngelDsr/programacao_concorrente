37ed2a9570c53b6c36bec569f94004a18df25da8
AngelDsr <92692918+AngelDsr@users.noreply.github.com>
2023-10-23 14:08:17 -0300

    Add files via upload

---
+++ laboratories/lab6/verificaLE.py
@@ -0,0 +1,52 @@
+#Define funcoes para verificar a logica de execucao de uma aplicacao leitor/escritor
+
+class LE:
+	def __init__(self):
+		self.escritores = 0
+		self.leitores = 0
+		self.escritorEsperando = 0
+
+	def leitorBloqueado(self,id):
+		'''Recebe o id do leitor. Verifica se a decisao de bloqueio esta correta.'''
+
+		if(self.escritores==0 and self.escritorEsperando==0):
+			print("ERRO: leitor " + str(id) + " bloqueado quando nao ha escritores!")
+
+	def escritorBloqueado(self,id):
+
+		'''Recebe o id do escritor. Verifica se a decisao de bloqueio esta correta.'''
+		if(self.escritores==0) and (self.leitores==0):
+			print("ERRO: escritor " + str(id) + " bloqueado quando nao ha escritores nem leitores!")
+
+		self.escritorEsperando += 1
+
+	def leitorLendo(self,id):
+		'''Recebe o id do leitor, verifica se pode ler e registra que esta lendo.'''
+
+		if(self.escritores>0 ):
+			print("ERRO: leitor " + str(id) + " esta lendo quando ha escritor escrevendo!")
+			
+		if (self.escritorEsperando > 0):
+			print("ERRO: leitor " + str(id) + " esta lendo quando ha escritor esperando para escrever!")
+
+		self.leitores+=1
+
+	def escritorEscrevendo(self,id):
+		'''Recebe o id do escritor, verifica se pode escrever e registra que esta escrevendo'''
+		if(self.escritores>0 or self.leitores>0):
+			print("ERRO: escritor " + str(id) + " esta escrevendo quando ha outro escritor ou leitores!")
+
+		if self.escritorEsperando != 0:
+			self.escritorEsperando -= 1
+
+		self.escritores+=1
+
+	def leitorSaindo(self,id):
+		'''Recebe o id do leitor e registra que terminou a leitura.'''
+		self.leitores-=1
+
+	def escritorSaindo(self,id):
+		'''Recebe o id do escritor e registra que terminou a leitura.'''
+		self.escritores-=1
+
+##acrescentar uma sequencia de escritas e leituras
